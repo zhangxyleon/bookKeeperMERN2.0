f83ccd302dc75ce956d011168989a478dba0897c
Xiangyu Zhang <leonfigocr17@gmail.com>
2022-04-06 08:44:21 -0400

    set mongodb connection

--- server.js
+++ server.js
@@ -2,6 +2,26 @@ const express = require('express');
 
 const app = express();
 
+//connet to Mongodb server
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+	try {
+		await mongoose.connect(db, {
+			useNewUrlParser: true,
+		});
+
+		console.log('MongoDB Connected...');
+	} catch (err) {
+		console.error(err.message);
+		process.exit(1);
+	}
+};
+
+connectDB();
+
 app.get('/', (req, res) => res.send('API Running'));
 
 const PORT = process.env.PORT || 3000;
